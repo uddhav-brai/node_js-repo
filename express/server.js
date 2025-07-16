d370604589a8f61b72a6596efa60f024dbb594e6
uddhav-brai <uddhavbhattarai007@gmail.com>
2025-07-17 00:13:12 +0545

    express lesson

---
+++ express/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { apiRouter } from './routes/apiRoutes.js'
+import cors from 'cors'
+
+const PORT = 8000
+
+const app = express()
+
+app.use(cors())
+
+app.use('/api', apiRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: "Endpoint not found. Please check the API documentation." })
+})
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
